94fdef4b55384f5ada637fc9006c42b46ba3c2f1
Паша <Pasha@macbook-air-pasa.local>
2016-10-06 12:40:52 +0300

    Initial Commit

---
+++ Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rich
+//
+//  Created by Паша on 06.10.16.
+//  Copyright © 2016 Паша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
